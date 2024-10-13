b3bc6ba459879f2702bbc7cff89e87a52f90ccb1
Ditaaa <113565619+ditanursabila@users.noreply.github.com>
2024-10-13 18:47:03 +0700

    Add files via upload

--- components/Header.vue
+++ components/Header.vue
@@ -3,7 +3,7 @@
     <header class="p-2 flex justify-center">
       <div class="row">
         <div class="w-20 me-40 flex items-center space-x-3">
-          <img src="assets/img/logosmk.png" class="logo" />
+          <img src="assets/img/logosmk.webp" class="logo" />
           <h1 class="whitespace-nowrap text-white text-3xl">Face Presensi</h1>
         </div>
       </div>
